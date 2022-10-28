59fb8e04090197f93711f622fd8df5114cf1a2d7
Tommy Alatalo <tommy.alatalo@gmail.com>
2022-10-28 18:05:33 +0000

    feat: 'issue board view' command support for group boards

--- commands/issue/board/view/issue_board_view.go
+++ commands/issue/board/view/issue_board_view.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"runtime/debug"
+	"strings"
 
 	"github.com/AlecAivazis/survey/v2"
 	"github.com/gdamore/tcell/v2"
@@ -16,13 +17,30 @@ import (
 )
 
 var (
-	apiClient     *gitlab.Client
-	project       *gitlab.Project
-	repo          glrepo.Interface
-	selectedBoard string
+	apiClient *gitlab.Client
+	project   *gitlab.Project
+	repo      glrepo.Interface
 )
 
+const closed string = "closed"
+const opened string = "opened"
+
+type issueBoardViewOptions struct {
+	assignee  string
+	labels    []string
+	milestone string
+	state     string
+}
+
+type boardMeta struct {
+	name    string
+	id      int
+	group   *gitlab.Group
+	project *gitlab.Project
+}
+
 func NewCmdView(f *cmdutils.Factory) *cobra.Command {
+	var opts = &issueBoardViewOptions{}
 	var viewCmd = &cobra.Command{
 		Use:   "view [flags]",
 		Short: `View project issue board.`,
@@ -32,8 +50,6 @@ func NewCmdView(f *cmdutils.Factory) *cobra.Command {
 
 			a := tview.NewApplication()
 			defer recoverPanic(a)
-
-			//out := utils.ColorableOut(cmd)
 
 			apiClient, err = f.HttpClient()
 			if err != nil {
@@ -50,68 +66,101 @@ func NewCmdView(f *cmdutils.Factory) *cobra.Command {
 				return fmt.Errorf("failed to get project: %w", err)
 			}
 
-			issueBoards, err := api.ListIssueBoards(apiClient, repo.FullName(), &gitlab.ListIssueBoardsOptions{})
-			if err != nil {
-				return fmt.Errorf("error retrieving issue board: %w", err)
-			}
-			boardStr := make([]string, len(issueBoards))
-			boardInfo := map[string]int{}
-			for i, board := range issueBoards {
-				boardStr[i] = board.Name
-				boardInfo[boardStr[i]] = board.ID
-			}
-
-			prompt := &survey.Select{
-				Message: "Select Board:",
-				Options: boardStr,
-			}
-			err = survey.AskOne(prompt, &selectedBoard)
+			// list the groups that are ancestors to project:
+			// https://docs.gitlab.com/ee/api/projects.html#list-a-projects-groups
+			projectGroups, err := api.ListProjectsGroups(
+				apiClient,
+				project.ID,
+				&gitlab.ListProjectGroupOptions{},
+			)
 			if err != nil {
 				return err
 			}
 
-			boadLists, err := api.GetIssueBoardLists(apiClient, repo.FullName(), boardInfo[selectedBoard], &gitlab.GetIssueBoardListsOptions{})
+			// get issue boards related to project and parent groups
+			// https://docs.gitlab.com/ee/api/group_boards.html#list-all-group-issue-boards-in-a-group
+			projectIssueBoards, err := getProjectIssueBoards()
 			if err != nil {
-				return err
+				return fmt.Errorf("getting project issue boards: %w", err)
+			}
+
+			projectGroupIssueBoards, err := getGroupIssueBoards(projectGroups, apiClient)
+			if err != nil {
+				return fmt.Errorf("getting project issue boards: %w", err)
+			}
+
+			// prompt user to select issue board
+			menuOptions, boardMetaMap := mapBoardData(projectIssueBoards, projectGroupIssueBoards)
+			selection, err := selectBoard(menuOptions)
+			if err != nil {
+				return fmt.Errorf("selecting issue board: %w", err)
+			}
+			selectedBoard := boardMetaMap[selection]
+
+			boardLists, err := getBoardLists(selectedBoard)
+			if err != nil {
+				return fmt.Errorf("getting issue board lists: %w", err)
 			}
 
 			root := tview.NewFlex()
-			var issues []*gitlab.Issue
-			// TODO: add `open` and `closed` board list. Both are not returned in the List API response payload
-			for _, list := range boadLists {
-				var boardIssues string
-				issues, err = api.ListIssues(apiClient, repo.FullName(), &gitlab.ListProjectIssuesOptions{
-					State:  gitlab.String("opened"),
-					Labels: &gitlab.Labels{list.Label.Name},
-				})
-				if err != nil {
-					return fmt.Errorf("error retrieving list issues: %w", err)
+			for _, l := range boardLists {
+				opts.state = ""
+				var boardIssues, listTitle, listColor string
+
+				if l.Label == nil {
+					continue
 				}
+
+				if l.Label != nil {
+					listTitle = l.Label.Name
+					listColor = l.Label.Color
+				}
+
+				// automatically request using state for default "open" and "closed" lists
+				// this is required as these lists aren't returned with the board lists api call
+				switch l.Label.Name {
+				case "Closed":
+					opts.state = closed
+				case "Open":
+					opts.state = opened
+				}
+
+				issues := []*gitlab.Issue{}
+				if selectedBoard.group != nil {
+					groupID := selectedBoard.group.ID
+					issues, err = getGroupBoardIssues(groupID, opts)
+					if err != nil {
+						return fmt.Errorf("getting issue board lists: %w", err)
+					}
+				}
+
+				if selectedBoard.group == nil {
+					issues, err = getProjectBoardIssues(opts)
+					if err != nil {
+						return fmt.Errorf("getting issue board lists: %w", err)
+					}
+				}
+
+				boardIssues = filterIssues(boardLists, issues, l, opts)
 				bx := tview.NewTextView().SetDynamicColors(true)
-				for _, issue := range issues {
-					var labelPrint string
-					var assignee string
-					totalLables := len(issue.Labels)
-					if totalLables > 0 {
-						for i, l := range issue.Labels {
-							if i == (totalLables - 1) {
-								labelPrint += l
-							} else {
-								labelPrint += l + ", "
-							}
-						}
-					}
-					if issue.Assignee != nil {
-						assignee = issue.Assignee.Username
-					}
-					boardIssues += fmt.Sprintf("[white]%s\n[blue](%s)\n[green]#%d[white] - %s\n\n", issue.Title, labelPrint, issue.IID, assignee)
-				}
 				bx.SetText(boardIssues).SetWrap(true)
-				bx.SetBorder(true).SetTitle(list.Label.Name).SetTitleColor(tcell.GetColor(list.Label.Color))
+				bx.SetBorder(true).SetTitle(listTitle).SetTitleColor(tcell.GetColor(listColor))
 				root.AddItem(bx, 0, 1, false)
 			}
 
-			root.SetBorderPadding(1, 1, 2, 2).SetBorder(true).SetTitle(fmt.Sprintf(" %s • Boards • %s ", selectedBoard, project.NameWithNamespace))
+			// format table title
+			var boardType, boardContext string
+			if selectedBoard.group != nil {
+				boardType = strings.Title("group")
+				boardContext = project.Namespace.Name
+			} else {
+				boardType = strings.Title("project")
+				boardContext = project.NameWithNamespace
+			}
+			root.SetBorderPadding(1, 1, 2, 2).SetBorder(true).SetTitle(
+				fmt.Sprintf(" %s • %s ", strings.Title(boardType+" issue board"), boardContext),
+			)
+
 			screen, err := tcell.NewScreen()
 			if err != nil {
 				return err
@@ -120,12 +169,67 @@ func NewCmdView(f *cmdutils.Factory) *cobra.Command {
 			if err := a.SetScreen(screen).SetRoot(root, true).Run(); err != nil {
 				return err
 			}
-
 			return nil
 		},
 	}
 
+	viewCmd.Flags().
+		StringVarP(&opts.assignee, "assignee", "a", "", "Filter board issues by assignee username")
+	viewCmd.Flags().
+		StringSliceVarP(&opts.labels, "labels", "l", []string{}, "Filter board issues by labels (comma separated)")
+	viewCmd.Flags().
+		StringVarP(&opts.milestone, "milestone", "m", "", "Filter board issues by milestone")
 	return viewCmd
+}
+
+func (opts *issueBoardViewOptions) getListProjectIssueOptions() *gitlab.ListProjectIssuesOptions {
+	withLabelDetails := true
+	reqOpts := &gitlab.ListProjectIssuesOptions{
+		WithLabelDetails: &withLabelDetails,
+	}
+
+	if opts.assignee != "" {
+		reqOpts.AssigneeUsername = &opts.assignee
+	}
+
+	if len(opts.labels) != 0 {
+		labels := gitlab.Labels(opts.labels)
+		reqOpts.Labels = &labels
+	}
+
+	if opts.state != "" {
+		reqOpts.State = &opts.state
+	}
+
+	if opts.milestone != "" {
+		reqOpts.Milestone = &opts.milestone
+	}
+	return reqOpts
+}
+
+func (opts *issueBoardViewOptions) getListGroupIssueOptions() *gitlab.ListGroupIssuesOptions {
+	withLabelDetails := true
+	reqOpts := &gitlab.ListGroupIssuesOptions{
+		WithLabelDetails: &withLabelDetails,
+	}
+
+	if opts.assignee != "" {
+		reqOpts.AssigneeUsername = &opts.assignee
+	}
+
+	if len(opts.labels) != 0 {
+		labels := gitlab.Labels(opts.labels)
+		reqOpts.Labels = &labels
+	}
+
+	if opts.state != "" {
+		reqOpts.State = &opts.state
+	}
+
+	if opts.milestone != "" {
+		reqOpts.Milestone = &opts.milestone
+	}
+	return reqOpts
 }
 
 func recoverPanic(app *tview.Application) {
@@ -133,4 +237,244 @@ func recoverPanic(app *tview.Application) {
 		app.Stop()
 		log.Fatalf("%s\n%s\n", r, string(debug.Stack()))
 	}
+}
+
+func buildLabelString(labelDetails []*gitlab.LabelDetails) string {
+	var labels string
+	for _, ld := range labelDetails {
+		labels += fmt.Sprintf("[white:%s:-]%s[white:-:-] ", ld.Color, ld.Name)
+	}
+	if labels != "" {
+		labels = strings.TrimSpace(labels) + fmt.Sprintf("\n")
+	}
+	return labels
+}
+
+func selectBoard(menuOptions []string) (string, error) {
+	var selectedOption string
+	prompt := &survey.Select{
+		Message: "Select board:",
+		Options: menuOptions,
+	}
+	err := survey.AskOne(prompt, &selectedOption)
+	if err != nil {
+		return "", err
+	}
+	return selectedOption, nil
+}
+
+// mapBoardData takes project and group issue board slices and
+// returns menu options for the user selection and a map of the board metadata keyed by the menu options
+func mapBoardData(
+	projectIssueBoards []*gitlab.IssueBoard,
+	projectGroupIssueBoards []*gitlab.GroupIssueBoard,
+) ([]string, map[string]boardMeta) {
+
+	// find longest board name to base padding on
+	maxNameLength := 0
+	for _, board := range projectIssueBoards {
+		if len(board.Name) > maxNameLength {
+			maxNameLength = len(board.Name)
+		}
+	}
+	for _, board := range projectGroupIssueBoards {
+		if len(board.Name) > maxNameLength {
+			maxNameLength = len(board.Name)
+		}
+	}
+
+	minPadding := 3
+	menuOptions := []string{}
+	boardMetaMap := map[string]boardMeta{}
+
+	formatMenuOption := func(boardName, parentName string, padding int, isGroupBoard bool) string {
+		sb := strings.Builder{}
+		sb.WriteString(boardName)
+		sb.WriteString(strings.Repeat(" ", padding))
+		kind := "PROJECT"
+		if isGroupBoard {
+			kind = "GROUP"
+		}
+		sb.WriteString(fmt.Sprintf("(%s: %s)", kind, parentName))
+		return sb.String()
+	}
+
+	// build menu entries and map metadata
+	for _, board := range projectGroupIssueBoards {
+		padding := minPadding
+		if maxNameLength-len(board.Name)+3 > minPadding {
+			padding = maxNameLength - len(board.Name) + 3
+		}
+
+		option := formatMenuOption(board.Name, board.Group.Name, padding, true)
+		menuOptions = append(menuOptions, option)
+		boardMetaMap[option] = boardMeta{
+			id:    board.ID,
+			name:  board.Name,
+			group: board.Group,
+		}
+	}
+
+	for _, board := range projectIssueBoards {
+		padding := minPadding
+		if maxNameLength-len(board.Name)+3 > minPadding {
+			padding = maxNameLength - len(board.Name) + 3
+		}
+
+		option := formatMenuOption(board.Name, board.Project.Name, padding, false)
+		menuOptions = append(menuOptions, option)
+		boardMetaMap[option] = boardMeta{
+			id:      board.ID,
+			name:    board.Name,
+			project: board.Project,
+		}
+	}
+	return menuOptions, boardMetaMap
+}
+
+func getProjectIssueBoards() ([]*gitlab.IssueBoard, error) {
+	projectIssueBoards, err := api.ListIssueBoards(
+		apiClient,
+		repo.FullName(),
+		&gitlab.ListIssueBoardsOptions{},
+	)
+	if err != nil {
+		return nil, fmt.Errorf("retrieving issue board: %w", err)
+	}
+	return projectIssueBoards, nil
+}
+
+func getGroupIssueBoards(
+	projectGroups []*gitlab.ProjectGroup,
+	gitlabClient *gitlab.Client,
+) ([]*gitlab.GroupIssueBoard, error) {
+	projectGroupIssueBoards := []*gitlab.GroupIssueBoard{}
+	for _, projectGroup := range projectGroups {
+		groupIssueBoards, err := api.ListGroupIssueBoards(
+			gitlabClient,
+			projectGroup.ID,
+			&gitlab.ListGroupIssueBoardsOptions{},
+		)
+		if err != nil {
+			return nil, fmt.Errorf("retrieving issue board: %w", err)
+		}
+		projectGroupIssueBoards = append(groupIssueBoards, projectGroupIssueBoards...)
+	}
+	return projectGroupIssueBoards, nil
+}
+
+func getBoardLists(board boardMeta) ([]*gitlab.BoardList, error) {
+	boardLists := []*gitlab.BoardList{}
+	var err error
+
+	if board.group != nil {
+		boardLists, err = api.GetGroupIssueBoardLists(apiClient, board.group.ID,
+			board.id, &gitlab.ListGroupIssueBoardListsOptions{})
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	if board.group == nil {
+		boardLists, err = api.GetIssueBoardLists(apiClient, repo.FullName(),
+			board.id, &gitlab.GetIssueBoardListsOptions{})
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	// add empty 'opened' and 'closed' lists before and after fetched lists
+	// these are used later when reading the issues into the table view
+	opened := &gitlab.BoardList{
+		Label: &gitlab.Label{
+			Name:      "Open",
+			Color:     "#fabd2f",
+			TextColor: "#000000",
+		},
+		Position: 0,
+	}
+	boardLists = append([]*gitlab.BoardList{opened}, boardLists...)
+
+	closed := &gitlab.BoardList{
+		Label: &gitlab.Label{
+			Name:      "Closed",
+			Color:     "#8ec07c",
+			TextColor: "#000000",
+		},
+		Position: len(boardLists),
+	}
+	boardLists = append(boardLists, closed)
+	return boardLists, nil
+}
+
+func getGroupBoardIssues(groupID int, opts *issueBoardViewOptions) ([]*gitlab.Issue, error) {
+	reqOpts := opts.getListGroupIssueOptions()
+	issues, err := api.ListGroupIssues(apiClient, groupID, reqOpts)
+	if err != nil {
+		return nil, fmt.Errorf("retrieving list issues: %w", err)
+	}
+	return issues, nil
+}
+
+func getProjectBoardIssues(opts *issueBoardViewOptions) ([]*gitlab.Issue, error) {
+	reqOpts := opts.getListProjectIssueOptions()
+	issues, err := api.ListIssues(apiClient, repo.FullName(), reqOpts)
+	if err != nil {
+		return nil, fmt.Errorf("retrieving list issues: %w", err)
+	}
+	return issues, nil
+}
+
+// filterIssues scans through the issues passed to it, filtering for the ones that belong in targetList
+// This function returns a string representation of the issues for targetList which will be displayed in the table view
+func filterIssues(
+	boardLists []*gitlab.BoardList,
+	issues []*gitlab.Issue,
+	targetList *gitlab.BoardList,
+	opts *issueBoardViewOptions,
+) string {
+	var boardIssues string
+next:
+	for _, issue := range issues {
+		switch opts.state {
+		// skip all issues that are not in the "closed" state for the "closed" list
+		case closed:
+			if issue.State != closed {
+				continue next
+			}
+		// skip issues labeled for other board lists when populating the "open" list
+		case opened:
+			for _, boardList := range boardLists {
+				for _, issueLabel := range issue.Labels {
+					if issueLabel == boardList.Label.Name {
+						continue next
+					}
+				}
+			}
+		// filter labeled issues into board lists with corresponding labels
+		default:
+			var hasListLabel bool
+			for _, issueLabel := range issue.Labels {
+				if issueLabel == targetList.Label.Name {
+					hasListLabel = true
+					break
+				}
+			}
+			if !hasListLabel || issue.State == closed {
+				continue next
+			}
+		}
+
+		var assignee, labelString string
+		if len(issue.Labels) > 0 {
+			labelString = buildLabelString(issue.LabelDetails)
+		}
+		if issue.Assignee != nil {
+			assignee = issue.Assignee.Username
+		}
+
+		boardIssues += fmt.Sprintf("[white::b]%s\n%s[green:-:-]#%d[darkgray] - %s\n\n",
+			issue.Title, labelString, issue.IID, assignee)
+	}
+	return boardIssues
 }
---
+++ commands/issue/board/view/issue_board_view_test.go
@@ -0,0 +1,318 @@
+package view
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/xanzy/go-gitlab"
+)
+
+func Test_issueBoardViewOptions_getListProjectIssueOptions(t *testing.T) {
+	withLabelDetails := true
+	labels := []string{"a", "b", "c"}
+	milestone := "milestone"
+	user := "user"
+	state := "open"
+	type fields struct {
+		assignee  string
+		labels    []string
+		milestone string
+		state     string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   *gitlab.ListProjectIssuesOptions
+	}{
+		{
+			name:   "return default values when passed empty options",
+			fields: fields{},
+			want: &gitlab.ListProjectIssuesOptions{
+				WithLabelDetails: &withLabelDetails,
+			},
+		},
+		{
+			name: "return corresponding values when passing options",
+			fields: fields{
+				assignee:  user,
+				labels:    labels,
+				milestone: milestone,
+				state:     state,
+			},
+			want: &gitlab.ListProjectIssuesOptions{
+				WithLabelDetails: &withLabelDetails,
+				AssigneeUsername: &user,
+				Milestone:        &milestone,
+				Labels:           &gitlab.Labels{"a", "b", "c"},
+				State:            &state,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			opts := &issueBoardViewOptions{
+				assignee:  tt.fields.assignee,
+				labels:    tt.fields.labels,
+				milestone: tt.fields.milestone,
+				state:     tt.fields.state,
+			}
+			got := opts.getListProjectIssueOptions()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
+
+func Test_issueBoardViewOptions_getListGroupIssueOptions(t *testing.T) {
+	withLabelDetails := true
+	labels := []string{"a", "b", "c"}
+	milestone := "milestone"
+	user := "user"
+	state := "open"
+	type fields struct {
+		assignee  string
+		labels    []string
+		milestone string
+		state     string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   *gitlab.ListGroupIssuesOptions
+	}{
+		{
+			name:   "return default values when passed empty options",
+			fields: fields{},
+			want: &gitlab.ListGroupIssuesOptions{
+				WithLabelDetails: &withLabelDetails,
+			},
+		},
+		{
+			name: "return corresponding values when passing options",
+			fields: fields{
+				assignee:  user,
+				labels:    labels,
+				milestone: milestone,
+				state:     state,
+			},
+			want: &gitlab.ListGroupIssuesOptions{
+				WithLabelDetails: &withLabelDetails,
+				AssigneeUsername: &user,
+				Milestone:        &milestone,
+				Labels:           &gitlab.Labels{"a", "b", "c"},
+				State:            &state,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			opts := &issueBoardViewOptions{
+				assignee:  tt.fields.assignee,
+				labels:    tt.fields.labels,
+				milestone: tt.fields.milestone,
+				state:     tt.fields.state,
+			}
+			got := opts.getListGroupIssueOptions()
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
+
+func Test_buildLabelString(t *testing.T) {
+	type args struct {
+		labelDetails []*gitlab.LabelDetails
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "return empty string if no labeldetails are passed",
+			args: args{[]*gitlab.LabelDetails{}},
+			want: "",
+		},
+		{
+			name: "return formatted string when labeldetails are passed",
+			args: args{[]*gitlab.LabelDetails{
+				{
+					Color: "blue",
+					Name:  "cold",
+				},
+				{
+					Color: "red",
+					Name:  "hot",
+				},
+			}},
+			want: "[white:blue:-]cold[white:-:-] [white:red:-]hot[white:-:-]\n",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := buildLabelString(tt.args.labelDetails)
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
+
+func Test_mapBoardData(t *testing.T) {
+	type args struct {
+		projectIssueBoards      []*gitlab.IssueBoard
+		projectGroupIssueBoards []*gitlab.GroupIssueBoard
+	}
+	type result struct {
+		menuOptions  []string
+		boardMetaMap map[string]boardMeta
+	}
+	tests := []struct {
+		name string
+		args args
+		want result
+	}{
+		{
+			name: "return empty map on empty inputs",
+			args: args{
+				projectIssueBoards:      []*gitlab.IssueBoard{},
+				projectGroupIssueBoards: []*gitlab.GroupIssueBoard{},
+			},
+			want: result{
+				menuOptions:  []string{},
+				boardMetaMap: map[string]boardMeta{},
+			},
+		},
+		{
+			name: "return metadata map with input values",
+			args: args{
+				projectIssueBoards: []*gitlab.IssueBoard{
+					{
+						Name:    "projectBoard",
+						Project: &gitlab.Project{Name: "project"},
+						ID:      1,
+					},
+				},
+				projectGroupIssueBoards: []*gitlab.GroupIssueBoard{
+					{
+						Name:  "groupBoard",
+						ID:    2,
+						Group: &gitlab.Group{Name: "group"},
+					},
+				},
+			},
+			want: result{
+				menuOptions: []string{
+					"groupBoard     (GROUP: group)",
+					"projectBoard   (PROJECT: project)",
+				},
+				boardMetaMap: map[string]boardMeta{
+					"projectBoard   (PROJECT: project)": {
+						id:      1,
+						name:    "projectBoard",
+						project: &gitlab.Project{Name: "project"},
+					},
+					"groupBoard     (GROUP: group)": {
+						id:    2,
+						name:  "groupBoard",
+						group: &gitlab.Group{Name: "group"},
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			menuOptions, boardMetaMap := mapBoardData(
+				tt.args.projectIssueBoards,
+				tt.args.projectGroupIssueBoards,
+			)
+			assert.Equal(t, tt.want.menuOptions, menuOptions)
+			assert.Equal(t, tt.want.boardMetaMap, boardMetaMap)
+		})
+	}
+}
+
+func Test_filterIssues(t *testing.T) {
+	type args struct {
+		boardLists []*gitlab.BoardList
+		issues     []*gitlab.Issue
+		targetList *gitlab.BoardList
+		opts       *issueBoardViewOptions
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "return empty string on no matches",
+			want: "",
+		},
+		{
+			name: "filter out closed issues when targetList is not the 'closed' list",
+			args: args{
+				boardLists: []*gitlab.BoardList{{Label: &gitlab.Label{Name: "A"}}},
+				issues:     []*gitlab.Issue{{Labels: []string{"A"}, State: "closed"}},
+				targetList: &gitlab.BoardList{Label: &gitlab.Label{Name: "A"}},
+				opts:       &issueBoardViewOptions{},
+			},
+			want: "",
+		},
+		{
+			name: "filter out issues not in the 'closed' state when populating the 'closed' list",
+			args: args{
+				boardLists: []*gitlab.BoardList{
+					{Label: &gitlab.Label{Name: "Closed"}},
+					{Label: &gitlab.Label{Name: "A"}},
+				},
+				issues:     []*gitlab.Issue{{Labels: []string{"A"}, State: "opened"}},
+				targetList: &gitlab.BoardList{Label: &gitlab.Label{Name: "Closed"}},
+				opts:       &issueBoardViewOptions{state: closed},
+			},
+			want: "",
+		},
+		{
+			name: "filter out issues labeled for other board lists when iterating over the 'open' list",
+			args: args{
+				boardLists: []*gitlab.BoardList{
+					{Label: &gitlab.Label{Name: "Open"}},
+					{Label: &gitlab.Label{Name: "A"}},
+				},
+				issues:     []*gitlab.Issue{{Labels: []string{"A"}, State: "opened"}},
+				targetList: &gitlab.BoardList{Label: &gitlab.Label{Name: "Open"}},
+				opts:       &issueBoardViewOptions{state: opened},
+			},
+			want: "",
+		},
+		{
+			name: "return formatted string on successful filter and match",
+			args: args{
+				boardLists: []*gitlab.BoardList{
+					{Label: &gitlab.Label{Name: "A"}},
+					{Label: &gitlab.Label{Name: "B"}},
+					{Label: &gitlab.Label{Name: "C"}},
+				},
+				issues: []*gitlab.Issue{
+					{
+						Assignee:     &gitlab.IssueAssignee{Username: "user"},
+						Labels:       []string{"A"},
+						LabelDetails: []*gitlab.LabelDetails{{Name: "A", Color: "green"}},
+						Title:        "Issue",
+						IID:          1,
+					},
+				},
+				targetList: &gitlab.BoardList{Label: &gitlab.Label{Name: "A"}},
+				opts:       &issueBoardViewOptions{},
+			},
+			want: "[white::b]Issue\n[white:green:-]A[white:-:-]\n[green:-:-]#1[darkgray] - user\n\n",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := filterIssues(
+				tt.args.boardLists,
+				tt.args.issues,
+				tt.args.targetList,
+				tt.args.opts,
+			)
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
