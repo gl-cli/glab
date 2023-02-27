4892732658ed419ee00b57ed2aa0155f374ec6bf
Jay McCure <jmccure@gitlab.com>
2023-02-28 09:10:46 +1000

    fix(issue create): better error message when users are not members

--- commands/issue/create/issue_create_test.go
+++ commands/issue/create/issue_create_test.go
@@ -2,15 +2,17 @@ package create
 
 import (
 	"fmt"
+	"net/http"
 	"strings"
 	"testing"
 	"time"
 
+	"gitlab.com/gitlab-org/cli/pkg/httpmock"
 	"gitlab.com/gitlab-org/cli/pkg/iostreams"
+	"gitlab.com/gitlab-org/cli/pkg/utils"
+	"gitlab.com/gitlab-org/cli/test"
 
 	"gitlab.com/gitlab-org/cli/pkg/prompt"
-
-	"gitlab.com/gitlab-org/cli/pkg/utils"
 
 	"github.com/acarl005/stripansi"
 	"github.com/stretchr/testify/assert"
@@ -115,4 +117,48 @@ func TestGenerateIssueWebURL(t *testing.T) {
 
 	assert.NoError(t, err)
 	assert.Equal(t, expectedUrl, u)
+}
+
+func runCommand(rt http.RoundTripper, isTTY bool, cli string) (*test.CmdOut, error) {
+	ios, _, stdout, stderr := cmdtest.InitIOStreams(isTTY, "")
+
+	factory := cmdtest.InitFactory(ios, rt)
+
+	_, _ = factory.HttpClient()
+
+	cmd := NewCmdCreate(factory)
+
+	return cmdtest.ExecuteCommand(cmd, cli, stdout, stderr)
+}
+
+func TestIssueCreateWhenIssuesDisabled(t *testing.T) {
+	fakeHTTP := &httpmock.Mocker{
+		MatchURL: httpmock.PathAndQuerystring,
+	}
+	defer fakeHTTP.Verify(t)
+
+	fakeHTTP.RegisterResponder(http.MethodGet, "/api/v4/projects/OWNER/REPO?license=true&statistics=true&with_custom_attributes=true",
+		httpmock.NewStringResponse(http.StatusOK, `{
+							  "id": 37777023,
+							  "description": "this is a test description",
+							  "name": "REPO",
+							  "name_with_namespace": "Test User / REPO",
+							  "path": "REPO",
+							  "path_with_namespace": "OWNER/REPO",
+							  "created_at": "2022-07-13T02:04:56.151Z",
+							  "default_branch": "main",
+							  "http_url_to_repo": "https://gitlab.com/OWNER/REPO.git",
+							  "web_url": "https://gitlab.com/OWNER/REPO",
+							  "readme_url": "https://gitlab.com/OWNER/REPO/-/blob/main/README.md",
+							  "issues_enabled": false
+							}`))
+
+	cli := `--title "test title" --description "test description"`
+
+	output, err := runCommand(fakeHTTP, false, cli)
+	assert.NotNil(t, err)
+	assert.Empty(t, output.String())
+	assert.Equal(t, "Issues are disabled for project \"OWNER/REPO\" or require project membership. "+
+		"Please ensure issues are enabled for the \"OWNER/REPO\" project, and if required, you are a member of the project.\n",
+		output.Stderr())
 }
