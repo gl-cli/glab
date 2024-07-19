906653ec5486817801f6995586f0c646b5bd606e
Alexey Shumkin <alex.crezoff@gmail.com>
2024-07-19 06:44:30 +0000

    fix(ci view): pipeline creator is shown incorrectly

--- commands/ci/view/view.go
+++ commands/ci/view/view.go
@@ -39,6 +39,7 @@ type ViewOpts struct {
 	Output    io.Writer
 
 	OpenInBrowser bool
+	PipelineUser  *gitlab.BasicUser
 }
 
 type ViewJobKind int64
@@ -171,6 +172,11 @@ func NewCmdView(f *cmdutils.Factory) *cobra.Command {
 				return utils.OpenInBrowser(webURL, browser)
 			}
 
+			p, err := api.GetSinglePipeline(opts.ApiClient, opts.Commit.LastPipeline.ID, opts.ProjectID)
+			if err != nil {
+				return fmt.Errorf("Can't get pipeline #%d info: %s", opts.Commit.LastPipeline.ID, err)
+			}
+			opts.PipelineUser = p.User
 			pipelines = make([]gitlab.PipelineInfo, 0, 10)
 
 			return drawView(opts)
@@ -190,7 +196,7 @@ func drawView(opts ViewOpts) error {
 		SetBackgroundColor(tcell.ColorDefault).
 		SetBorderPadding(1, 1, 2, 2).
 		SetBorder(true).
-		SetTitle(fmt.Sprintf(" Pipeline #%d triggered %s by %s ", opts.Commit.LastPipeline.ID, utils.TimeToPrettyTimeAgo(*opts.Commit.LastPipeline.CreatedAt), opts.Commit.AuthorName))
+		SetTitle(fmt.Sprintf(" Pipeline #%d triggered %s by %s ", opts.Commit.LastPipeline.ID, utils.TimeToPrettyTimeAgo(*opts.Commit.LastPipeline.CreatedAt), opts.PipelineUser.Name))
 
 	boxes = make(map[string]*tview.TextView)
 	jobsCh := make(chan []*ViewJob)
