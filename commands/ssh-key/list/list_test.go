3de243302af2cf96178798bac5296d9e12d77e53
Filip Aleksic <faleksic@gitlab.com>
2025-02-19 09:08:45 +0000

    feat(ssh-key): add usage type to output

--- commands/ssh-key/list/list_test.go
+++ commands/ssh-key/list/list_test.go
@@ -58,7 +58,7 @@ func TestSSHKeyList(t *testing.T) {
 				http.StatusOK,
 				keyResponse,
 			}},
-			expectedOut: "Title\tKey\tCreated At\nkey title\tssh-ed25519 example\t2025-01-01 00:00:00 +0000 UTC\n\n",
+			expectedOut: "Title\tKey\tUsage type\tCreated At\nkey title\tssh-ed25519 example\tauth_and_signing\t2025-01-01 00:00:00 +0000 UTC\n\n",
 		},
 	}
 
