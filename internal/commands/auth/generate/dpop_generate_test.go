6e9f8a8de73816b88e08ace051c8f8f4aef6e2bf
Timo Furrer <tuxtimo@gmail.com>
2025-07-01 13:23:46 +0200

    refactor: speed up dpop generate test by pre-generating test data
    This test data is very expensive to generate - approx. 1 minute on my
    machine for generating a single key for each type.
    
    It was by far the slowest test. Now, it takes a couple of millis.

--- internal/commands/auth/generate/dpop_generate_test.go
+++ internal/commands/auth/generate/dpop_generate_test.go
@@ -2,30 +2,69 @@ package generate
 
 import (
 	"crypto"
-	"crypto/ecdsa"
 	"crypto/elliptic"
-	"crypto/rand"
 	"crypto/rsa"
+	"path/filepath"
 	"testing"
 
 	"github.com/golang-jwt/jwt/v5"
 	"github.com/stretchr/testify/assert"
-	"golang.org/x/crypto/ed25519"
+	"github.com/stretchr/testify/require"
 )
 
-func getEd25519PrivateKey() crypto.PrivateKey {
-	_, pk, _ := ed25519.GenerateKey(rand.Reader)
-	return &pk
+//go:generate go run testdata/generate_test_keys.go
+
+// Helper function to load pre-generated test keys
+func loadTestKey(t *testing.T, filename string) crypto.PrivateKey {
+	t.Helper()
+
+	key, err := loadPrivateKey(filepath.Join("testdata", filename), nil)
+	require.NoError(t, err)
+	return key
 }
 
-func getEcdsaPrivateKey(curve elliptic.Curve) crypto.PrivateKey {
-	pk, _ := ecdsa.GenerateKey(curve, rand.Reader)
-	return pk
+func getEd25519PrivateKey(t *testing.T) crypto.PrivateKey {
+	t.Helper()
+
+	return loadTestKey(t, "ed25519_key.pem")
 }
 
-func getRsaPrivateKey(bits int) crypto.PrivateKey {
-	pk, _ := rsa.GenerateKey(rand.Reader, bits)
-	return pk
+func getEcdsaPrivateKey(t *testing.T, curve elliptic.Curve) crypto.PrivateKey {
+	t.Helper()
+
+	var filename string
+	switch curve {
+	case elliptic.P256():
+		filename = "ecdsa_p256_key.pem"
+	case elliptic.P384():
+		filename = "ecdsa_p384_key.pem"
+	case elliptic.P521():
+		filename = "ecdsa_p521_key.pem"
+	case elliptic.P224():
+		filename = "ecdsa_p224_key.pem"
+	default:
+		t.Fatalf("unsupported ecdsa key for curve %v", curve)
+	}
+	return loadTestKey(t, filename)
+}
+
+func getRsaPrivateKey(t *testing.T, bits int) crypto.PrivateKey {
+	t.Helper()
+
+	var filename string
+	switch bits {
+	case 2047:
+		filename = "rsa_2047_key.pem"
+	case 8193:
+		filename = "rsa_8193_key.pem"
+	case 2048:
+		filename = "rsa_2048_key.pem"
+	case 8192:
+		filename = "rsa_8192_key.pem"
+	default:
+		t.Fatalf("unsupported RSA key size %d", bits)
+	}
+	return loadTestKey(t, filename)
 }
 
 func TestSigningMethods(t *testing.T) {
@@ -35,39 +74,39 @@ func TestSigningMethods(t *testing.T) {
 		shouldError           bool
 	}{
 		{
-			key:                   getEd25519PrivateKey(),
+			key:                   getEd25519PrivateKey(t),
 			expectedSigningMethod: jwt.SigningMethodEdDSA,
 		},
 		{
-			key:         getEcdsaPrivateKey(elliptic.P256()),
+			key:         getEcdsaPrivateKey(t, elliptic.P256()),
 			shouldError: true, // GitLab only support the _sk variant which isn't supported by glab yet
 		},
 		{
-			key:         getEcdsaPrivateKey(elliptic.P384()),
+			key:         getEcdsaPrivateKey(t, elliptic.P384()),
 			shouldError: true, // GitLab only support the _sk variant which isn't supported by glab yet
 		},
 		{
-			key:         getEcdsaPrivateKey(elliptic.P521()),
+			key:         getEcdsaPrivateKey(t, elliptic.P521()),
 			shouldError: true, // GitLab only support the _sk variant which isn't supported by glab yet
 		},
 		{
-			key:         getEcdsaPrivateKey(elliptic.P224()),
+			key:         getEcdsaPrivateKey(t, elliptic.P224()),
 			shouldError: true, // ssh-keygen doesn't do 224 bit keys
 		},
 		{
-			key:         getRsaPrivateKey(2047),
+			key:         getRsaPrivateKey(t, 2047),
 			shouldError: true,
 		},
 		{
-			key:         getRsaPrivateKey(8193),
+			key:         getRsaPrivateKey(t, 8193),
 			shouldError: true,
 		},
 		{
-			key:                   getRsaPrivateKey(2048),
+			key:                   getRsaPrivateKey(t, 2048),
 			expectedSigningMethod: jwt.SigningMethodRS512,
 		},
 		{
-			key:                   getRsaPrivateKey(8192),
+			key:                   getRsaPrivateKey(t, 8192),
 			expectedSigningMethod: jwt.SigningMethodRS512,
 		},
 		{
---
+++ internal/commands/auth/generate/testdata/generate_test_keys.go
@@ -0,0 +1,162 @@
+package main
+
+import (
+	"bytes"
+	"crypto"
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+
+	"golang.org/x/crypto/ed25519"
+	"golang.org/x/crypto/ssh"
+)
+
+func main() {
+	// Create testdata directory if it doesn't exist
+	testdataDir := "testdata"
+	if err := os.MkdirAll(testdataDir, 0755); err != nil {
+		log.Fatalf("Failed to create testdata directory: %v", err)
+	}
+
+	// Generate Ed25519 key
+	if err := generateEd25519Key(filepath.Join(testdataDir, "ed25519_key.pem")); err != nil {
+		log.Fatalf("Failed to generate Ed25519 key: %v", err)
+	}
+
+	// Generate ECDSA keys for different curves
+	curves := []struct {
+		name  string
+		curve elliptic.Curve
+	}{
+		{"ecdsa_p256", elliptic.P256()},
+		{"ecdsa_p384", elliptic.P384()},
+		{"ecdsa_p521", elliptic.P521()},
+	}
+
+	for _, c := range curves {
+		filename := filepath.Join(testdataDir, c.name+"_key.pem")
+		if err := generateEcdsaKey(filename, c.curve); err != nil {
+			log.Fatalf("Failed to generate ECDSA key for %s: %v", c.name, err)
+		}
+	}
+
+	// Generate P-224 key separately since it's not supported by SSH
+	if err := generateEcdsaKeyManual(filepath.Join(testdataDir, "ecdsa_p224_key.pem"), elliptic.P224()); err != nil {
+		log.Fatalf("Failed to generate ECDSA P-224 key: %v", err)
+	}
+
+	// Generate RSA keys with different bit sizes
+	rsaSizes := []struct {
+		name string
+		bits int
+	}{
+		{"rsa_2047", 2047},
+		{"rsa_8193", 8193},
+		{"rsa_2048", 2048},
+		{"rsa_8192", 8192},
+	}
+
+	for _, r := range rsaSizes {
+		filename := filepath.Join(testdataDir, r.name+"_key.pem")
+		if err := generateRsaKey(filename, r.bits); err != nil {
+			log.Fatalf("Failed to generate RSA key for %s: %v", r.name, err)
+		}
+	}
+}
+
+func generateEd25519Key(filename string) error {
+	_, privateKey, err := ed25519.GenerateKey(rand.Reader)
+	if err != nil {
+		return err
+	}
+
+	return savePrivateKeyAsSSH(filename, privateKey)
+}
+
+func generateEcdsaKey(filename string, curve elliptic.Curve) error {
+	privateKey, err := ecdsa.GenerateKey(curve, rand.Reader)
+	if err != nil {
+		return err
+	}
+
+	return savePrivateKeyAsSSH(filename, privateKey)
+}
+
+func generateEcdsaKeyManual(filename string, curve elliptic.Curve) error {
+	privateKey, err := ecdsa.GenerateKey(curve, rand.Reader)
+	if err != nil {
+		return err
+	}
+
+	return savePrivateKeyManual(filename, privateKey)
+}
+
+func generateRsaKey(filename string, bits int) error {
+	privateKey, err := rsa.GenerateKey(rand.Reader, bits)
+	if err != nil {
+		return err
+	}
+
+	return savePrivateKeyAsSSH(filename, privateKey)
+}
+
+func savePrivateKeyAsSSH(filename string, privateKey crypto.PrivateKey) error {
+	// Convert to SSH format
+	sshPrivateKey, err := ssh.MarshalPrivateKey(privateKey, "")
+	if err != nil {
+		return err
+	}
+
+	// Write to file
+	encoded := &bytes.Buffer{}
+	err = pem.Encode(encoded, sshPrivateKey)
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(filename, encoded.Bytes(), os.FileMode(os.O_CREATE))
+}
+
+func savePrivateKeyManual(filename string, privateKey crypto.PrivateKey) error {
+	var keyBytes []byte
+	var keyType string
+	var err error
+
+	switch key := privateKey.(type) {
+	case *rsa.PrivateKey:
+		keyBytes = x509.MarshalPKCS1PrivateKey(key)
+		keyType = "RSA PRIVATE KEY"
+	case *ecdsa.PrivateKey:
+		keyBytes, err = x509.MarshalECPrivateKey(key)
+		keyType = "EC PRIVATE KEY"
+	case ed25519.PrivateKey:
+		keyBytes, err = x509.MarshalPKCS8PrivateKey(key)
+		keyType = "PRIVATE KEY"
+	default:
+		return fmt.Errorf("unsupported key type: %T", privateKey)
+	}
+
+	if err != nil {
+		return err
+	}
+
+	pemBlock := &pem.Block{
+		Type:  keyType,
+		Bytes: keyBytes,
+	}
+
+	encoded := &bytes.Buffer{}
+	err = pem.Encode(encoded, pemBlock)
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(filename, encoded.Bytes(), os.FileMode(os.O_CREATE))
+}
