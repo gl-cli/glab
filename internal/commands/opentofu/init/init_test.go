40a67120248d0ea87c5a72915a54376920296516
Timo Furrer <tfurrer@gitlab.com>
2025-09-08 18:58:29 +0200

    feat(opentofu init): support OAuth2 token auth
    This change set adds support for OAuth2 tokens authentications for the
    opentofu init command.
    
    Closes https://gitlab.com/gitlab-org/cli/-/issues/7978

--- internal/commands/opentofu/init/init_test.go
+++ internal/commands/opentofu/init/init_test.go
@@ -12,7 +12,6 @@ import (
 	"gitlab.com/gitlab-org/cli/internal/api"
 	"gitlab.com/gitlab-org/cli/internal/cmdutils"
 	"gitlab.com/gitlab-org/cli/internal/testing/cmdtest"
-	"go.uber.org/mock/gomock"
 )
 
 func TestInit_CommandConstruction(t *testing.T) {
@@ -28,18 +27,16 @@ func TestInit_CommandConstruction(t *testing.T) {
 				"-backend-config=address=https://gitlab.example.com/api/v4/projects/OWNER%2FREPO/terraform/state/production",
 				"-backend-config=lock_address=https://gitlab.example.com/api/v4/projects/OWNER%2FREPO/terraform/state/production/lock",
 				"-backend-config=unlock_address=https://gitlab.example.com/api/v4/projects/OWNER%2FREPO/terraform/state/production/lock",
-				"-backend-config=username=testuser",
-				"-backend-config=password=testtoken",
 				"-backend-config=lock_method=POST",
 				"-backend-config=unlock_method=DELETE",
 				"-backend-config=retry_wait_min=5",
+				"-backend-config=headers={\"Authorization\" = \"Bearer testtoken\"}",
 			}))
 		},
 		false,
 		cmdtest.WithApiClient(cmdtest.NewTestApiClient(t, nil, "testtoken", "gitlab.example.com", api.WithGitLabClient(tc.Client))),
 		cmdtest.WithBaseRepo("OWNER", "REPO", "gitlab.example.com"),
 	)
-	tc.MockUsers.EXPECT().CurrentUser(gomock.Any()).Return(&gitlab.User{Username: "testuser"}, nil, nil)
 
 	// WHEN
 	_, err := exec("production -d infra -b my-tofu")
@@ -59,11 +56,10 @@ func TestInit_CommandConstruction_InitArgs(t *testing.T) {
 				"-backend-config=address=https://gitlab.example.com/api/v4/projects/OWNER%2FREPO/terraform/state/production",
 				"-backend-config=lock_address=https://gitlab.example.com/api/v4/projects/OWNER%2FREPO/terraform/state/production/lock",
 				"-backend-config=unlock_address=https://gitlab.example.com/api/v4/projects/OWNER%2FREPO/terraform/state/production/lock",
-				"-backend-config=username=testuser",
-				"-backend-config=password=testtoken",
 				"-backend-config=lock_method=POST",
 				"-backend-config=unlock_method=DELETE",
 				"-backend-config=retry_wait_min=5",
+				"-backend-config=headers={\"Authorization\" = \"Bearer testtoken\"}",
 				"-reconfigure",
 			}))
 		},
@@ -71,7 +67,6 @@ func TestInit_CommandConstruction_InitArgs(t *testing.T) {
 		cmdtest.WithApiClient(cmdtest.NewTestApiClient(t, nil, "testtoken", "gitlab.example.com", api.WithGitLabClient(tc.Client))),
 		cmdtest.WithBaseRepo("OWNER", "REPO", "gitlab.example.com"),
 	)
-	tc.MockUsers.EXPECT().CurrentUser(gomock.Any()).Return(&gitlab.User{Username: "testuser"}, nil, nil)
 
 	// WHEN
 	_, err := exec("production -d infra -b my-tofu -- -reconfigure")
