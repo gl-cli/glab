b5e22ee182d6e0659732002a374eabe815972a7b
Timo Furrer <tfurrer@gitlab.com>
2025-09-08 14:36:37 +0200

    fix(debug http): deep-copy body for request dump
    This change fixes an issue where the body wasn't deep copied for the
    sensitive value sanitization when dumping HTTP requests.
    
    Closes https://gitlab.com/gitlab-org/cli/-/issues/7963

--- internal/api/debug_transport.go
+++ internal/api/debug_transport.go
@@ -1,6 +1,8 @@
 package api
 
 import (
+	"bytes"
+	"context"
 	"fmt"
 	"io"
 	"net/http"
@@ -21,7 +23,12 @@ type debugTransport struct {
 }
 
 func (d *debugTransport) RoundTrip(req *http.Request) (*http.Response, error) {
-	reqDump, err := httputil.DumpRequestOut(redactKnownSensitiveHeaders(req), true)
+	reqToDump, err := redactKnownSensitiveHeaders(req)
+	if err != nil {
+		return nil, err
+	}
+
+	reqDump, err := httputil.DumpRequestOut(reqToDump, true)
 	if err != nil {
 		return nil, err
 	}
@@ -43,12 +50,43 @@ func (d *debugTransport) RoundTrip(req *http.Request) (*http.Response, error) {
 	return resp, nil
 }
 
-func redactKnownSensitiveHeaders(req *http.Request) *http.Request {
-	cloned := req.Clone(req.Context())
+func redactKnownSensitiveHeaders(req *http.Request) (*http.Request, error) {
+	r1, r2, err := drainBody(req.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	cloned := req.Clone(context.Background())
+	cloned.Body = r1
+	req.Body = r2
+
 	for _, h := range sensitiveHeaders {
 		if cloned.Header.Get(h) != "" {
 			cloned.Header.Set(h, "[REDACTED]")
 		}
 	}
-	return cloned
+	return cloned, nil
+}
+
+// The code below is copied from net/http/httputil/dump.go
+// Copyright 2009 The Go Authors. All rights reserved.
+
+// drainBody reads all of b to memory and then returns two equivalent
+// ReadClosers yielding the same bytes.
+//
+// It returns an error if the initial slurp of all bytes fails. It does not attempt
+// to make the returned ReadClosers have identical error-matching behavior.
+func drainBody(b io.ReadCloser) (r1, r2 io.ReadCloser, err error) { //nolint:nonamedreturns
+	if b == nil || b == http.NoBody {
+		// No copying needed. Preserve the magic sentinel meaning of NoBody.
+		return http.NoBody, http.NoBody, nil
+	}
+	var buf bytes.Buffer
+	if _, err = buf.ReadFrom(b); err != nil {
+		return nil, b, err
+	}
+	if err = b.Close(); err != nil {
+		return nil, b, err
+	}
+	return io.NopCloser(&buf), io.NopCloser(bytes.NewReader(buf.Bytes())), nil
 }
